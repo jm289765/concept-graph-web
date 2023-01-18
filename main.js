2618dfa834d393dc1e12f9b1a751c88af48ad0eb
jm289765 <112527928+jm289765@users.noreply.github.com>
2023-01-18 03:06:45 +0000

    button to make new node
    also more error checking in NodeList
    removed jsplumb, i wasn't using it and don't need it

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
-import {jsPlumbBrowserUI} from "./modules/jsplumb.js"
 import {HttpRequests as requests} from "./httprequests.js";
 
 class NodeList {
     _nodeList = {} // dict<int, jsonData>
 
     validId(id) {
-        return true // todo: this doesn't work
-        // id is not empty string and id only contains numbers
+        /*
+        true if id is positive integer or string containing positive integer.
+
+        else false.
+         */
+        if (id === undefined) {
+            return false
+        }
+
         const strId = id.toString()
-        return strId !== "" && strId.match(/^\d+$/)
+        return strId !== "" && strId.match(/^\d+$/) !== null
     }
 
     async get(id="0") {
+        /*
+        returns json object of node with given id.
+
+        if the nodeList doesn't have the node, it will attempt to retrieve it from the server.
+        if the server doesn't have the node, or an invalid id is provided, this will return false.
+         */
         if (!this.validId(id))
             return false
 
@@ -19,7 +31,7 @@ class NodeList {
             return this._nodeList[id]
         } else {
             await this.update(id)
-            if (id in this._nodeList) {
+            if (this.has(id)) {
                 return this._nodeList[id]
             } else {
                 return false
@@ -28,6 +40,11 @@ class NodeList {
     }
 
     put(id, json) {
+        /*
+        adds a key-value pair to the underlying dictionary: {id: json}.
+
+        json should be a node's json object, e.g. {"id": "0", "title": "root", "content": "", "type": "root"}
+         */
         if (!this.validId(id))
             return false
 
@@ -35,19 +52,32 @@ class NodeList {
     }
 
     has(id) {
+        /*
+        returns true if the id is in the node list. be aware that NodeList.get(id) handles this automatically.
+         */
         return id in this._nodeList
     }
 
     async update(id) {
+        /*
+        retrieves node "id" from the server, then adds it to the node list.
+
+        returns false if node id is invalid. returns true if successful.
+         */
         if (!this.validId(id))
             return false
 
         const _rootNodeJSON = await requests.getNode(id)
-        // todo: make sure to remove deleted nodes from nodeList
-        console.log(_rootNodeJSON)
         if (_rootNodeJSON) {
             singleNodeFromJSON(_rootNodeJSON)
+            return true
         }
+
+        return false
+    }
+
+    async delete(id) {
+        // todo: make sure to remove deleted nodes from nodeList
     }
 }
 
@@ -60,6 +90,7 @@ function nodesFromJSON(nodeJSON) {
 }
 
 function singleNodeFromJSON(nodeJSON) {
+    // might be better to put this inside NodeList
     nodeList.put(nodeJSON.id, nodeJSON)
 }
 
@@ -71,51 +102,61 @@ async function listAllButtonFunc() {
     nodesFromJSON(graph.nodes)
 }
 
-async function addNodeButtonFunc() {
-    const addNodeText = document.getElementById("add-node-text")
-    const newNodeId = await requests.addNode("comment", "Made by 'Add Node'")
-    addNodeText.innerText = JSON.stringify(await nodeList.get(newNodeId))
-    await updateDisplayedNode(newNodeId)
-}
-
 async function updateDisplayedNode(id=null) {
+    /*
+    display the node specified by `id` param. if id is null, display node specified by
+    the "node-input" input box.
+
+    handles all things necessary to change the displayed node.
+     */
     const nodeInput = document.getElementById("node-input")
+    const titleInput = document.getElementById("title-input")
+    const contentInput = document.getElementById("content-input")
+    const titleDisplay = document.getElementById("selected-node-view")
 
     if (id !== null) {
         nodeInput.value = id
     }
 
+    if (nodeInput.value === "0") { // can't edit root node
+        titleInput.setAttribute("disabled", "")
+        contentInput.setAttribute("disabled", "")
+    } else {
+        titleInput.removeAttribute("disabled")
+        contentInput.removeAttribute("disabled")
+    }
+
     const node = await nodeList.get(nodeInput.value)
     if (node) {
-        const titleInput = document.getElementById("title-input")
-        const contentInput = document.getElementById("content-input")
-        const titleDisplay = document.getElementById("selected-node-view")
-
         titleInput.value = node["title"]
         contentInput.value = node["content"]
-        titleDisplay.innerText = node["title"]
+        // todo: move the node id display to a different element
+        // todo: better way to handle excessively long titles
+        titleDisplay.innerText = `[${node["id"]}] ${node["title"]}`
+    } else { // invalid node
+        titleInput.value = ""
+        contentInput.value = ""
+        if(nodeInput.value === "") {
+            titleDisplay.innerText = "[] No node selected."
+        } else {
+            titleDisplay.innerText = "[] Node does not exist."
+        }
     }
 }
 
-async function saveButtonFunc() {
+async function updateButtonFunc() {
     /*
-    if nodeInput.value not in nodeList, try to get it from server (nodeList might handle this automatically)
-    if the server doesn't have that id, then send an add request
-
-    else if nodeInput.value in nodeList, send an update request for title and content if they're different
-    than nodeList's copy of them
+    tell the server to set the selected node's attributes to the values in
+    the editor boxes. if the values are already accurate, no change.
      */
     const nodeInput = document.getElementById("node-input")
     const titleInput = document.getElementById("title-input")
     const contentInput = document.getElementById("content-input")
     const nodeId = nodeInput.value
 
-    const node = nodeList.get(nodeId)
+    const node = await nodeList.get(nodeId)
     if (!node) {
-        // todo: node type support
-        // todo: this doesn't work
-        const newNodeId = await requests.addNode("concept", titleInput.value, contentInput.value)
-        await updateDisplayedNode(newNodeId)
+        // todo: error message?
     }
     else {
         let u = false
@@ -131,8 +172,21 @@ async function saveButtonFunc() {
 
         if (u) {
             await nodeList.update(nodeId)
+            await updateDisplayedNode()
         }
     }
+}
+
+async function createButtonFunc() {
+    /*
+    creates a new node using the title and content from their respective input boxes. then displays the new node.
+     */
+    const titleInput = document.getElementById("title-input")
+    const contentInput = document.getElementById("content-input")
+
+    // todo: support for user-specified node type
+    const newNodeId = await requests.addNode("concept", titleInput.value, contentInput.value)
+    await updateDisplayedNode(newNodeId)
 }
 
 async function init() {
@@ -140,13 +194,13 @@ async function init() {
 
     const nodeInput = document.getElementById("node-input")
     nodeInput.oninput = () => updateDisplayedNode()
-    const saveButton = document.getElementById("save-button")
-    saveButton.onclick = saveButtonFunc
+    const saveButton = document.getElementById("update-button")
+    saveButton.onclick = updateButtonFunc
+    const createButton = document.getElementById("create-button")
+    createButton.onclick = createButtonFunc
 
     const listAllButton = document.getElementById("list-all-nodes-button")
     listAllButton.onclick = listAllButtonFunc
-    const addNodeButton = document.getElementById("add-node-button")
-    addNodeButton.onclick = addNodeButtonFunc
 }
 
 init()
